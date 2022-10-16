a4c121581844f3ebaa7d37a10afb65d1896fffda
Panut <63021710@up.ac.th>
2022-10-17 03:24:24 +0700

    Dev myself page and history page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './history/history.component';
+import { MyselfComponent } from './myself/myself.component';
+import { SkillComponent } from './skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-myself', component: MyselfComponent },
+  { path: 'app-history', component: HistoryComponent },
+  { path: 'app-skill', component: SkillComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MyselfComponent } from './myself/myself.component';
+import { HistoryComponent } from './history/history.component';
+import { SkillComponent } from './skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MyselfComponent,
+    HistoryComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
