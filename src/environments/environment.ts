579e60b1296a81cb07d9529fe080aa7c9e07a9b2
Panut <63021710@up.ac.th>
2022-10-17 08:31:51 +0700

    Install firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,23 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { initializeApp } from 'firebase/app';
 
 export const environment = {
-  production: false
+  
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBQepJk7JMVLjdd4xoynG6zAbhdNH1rDVE",
+    authDomain: "cv-2022-b75f5.firebaseapp.com",
+    projectId: "cv-2022-b75f5",
+    storageBucket: "cv-2022-b75f5.appspot.com",
+    messagingSenderId: "456222256345",
+    appId: "1:456222256345:web:d88428d1f254a1e672a2c8",
+    measurementId: "G-1F82Q8EL1X"
+  }
 };
+
+const app = initializeApp(environment.firebaseConfig);
 
 /*
  * For easier debugging in development mode, you can import the following file
